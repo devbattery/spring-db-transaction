3f18f15a804dee44f14ac604d27fe40e39a8cedd
Sully <2dcoder@naver.com>
2024-07-05 20:08:22 +0900

    섹션 9: 스프링 트랜잭션 이해
    트랜잭션 AOP 주의 사항 - 프록시 내부 호출 1

---
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,81 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @DisplayName("")
+    @Test
+    void printProxy() {
+        // given
+        // when
+        // then
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @DisplayName("")
+    @Test
+    void internalCall() {
+        // given
+        // when
+        // then
+        callService.internal();
+    }
+
+    @DisplayName("")
+    @Test
+    void externalCall() {
+        // given
+        // when
+        // then
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal();
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+
+    }
+
+}
