7f8bd3b66a664fd8e6028b671dd72ba0f7ba79eb
Sully <2dcoder@naver.com>
2024-07-08 21:22:53 +0900

    섹션 10: 스프링 트랜잭션 전파 1 - 기본
    스프링 트랙잭션 전파 5 - 외부 롤백

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -114,7 +114,28 @@ public class BasicTxTest {
         txManager.commit(inner);
 
         log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("")
+    @Test
+    void outer_rollback() {
+        // given
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 커밋");
         txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
 
         // when
 
